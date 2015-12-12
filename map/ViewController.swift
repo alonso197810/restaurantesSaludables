ca25daf7fc8cdc0ae8ade85fcc488cc78f2ec389
Jimmy A. Espinoza <jimboea@macbook-air-de-jimmy.local>
2015-12-12 17:15:00 -0500

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Jimmy A. Espinoza  on 12/12/15.
+//  Copyright © 2015 Jimmy A. Espinoza . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
